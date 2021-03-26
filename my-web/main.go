91b9b8518360f0dd7533d2b15e4471ea937ae237
hanzq <1608085113@qq.com>
2021-03-26 21:56:37 +0800

    完善

---
+++ my-web/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+func main() {
+	r := gin.Default()
+	r.GET("/", func(c *gin.Context) {
+		c.String(http.StatusOK, "hello world, this is v2 version")
+	})
+
+	r.Run(":8080")
+}
+
+
+
